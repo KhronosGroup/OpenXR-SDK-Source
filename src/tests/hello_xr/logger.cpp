95ba6c3fd9d2eaf619913e48d416ab7ef4f7bcbe
Andrew Eikum <aeikum@codeweavers.com>
2020-10-07 14:42:40 -0500

    hello_xr: Check for Windows with _WIN32
    Fixes building with mingw-w64, which does not define _MSC_VER.

--- src/tests/hello_xr/logger.cpp
+++ src/tests/hello_xr/logger.cpp
@@ -28,7 +28,7 @@ void Write(Level severity, const std::string& msg) {
     const auto now = std::chrono::system_clock::now();
     const time_t now_time = std::chrono::system_clock::to_time_t(now);
     tm now_tm;
-#ifdef _MSC_VER
+#ifdef _WIN32
     localtime_s(&now_tm, &now_time);
 #else
     localtime_r(&now_time, &now_tm);
