f0d8425ae2946472e178ae0fac6a2c03dcc6fba9
Christoph Haag <christoph.haag@collabora.com>
2021-06-22 00:38:58 +0200

    layers/core_validation: Default to stdout logging
    Vulkan validation defaults to stdout logging too.

--- src/api_layers/core_validation.cpp
+++ src/api_layers/core_validation.cpp
@@ -481,13 +481,15 @@ XRAPI_ATTR XrResult XRAPI_CALL CoreValidationXrCreateApiLayerInstance(const XrIn
 
         if (!g_record_info.initialized) {
             g_record_info.initialized = true;
-            g_record_info.type = RECORD_NONE;
+            g_record_info.type = RECORD_TEXT_COUT;
         }
 
         std::string export_type = PlatformUtilsGetEnv("XR_CORE_VALIDATION_EXPORT_TYPE");
         std::string file_name = PlatformUtilsGetEnv("XR_CORE_VALIDATION_FILE_NAME");
         if (!file_name.empty()) {
             g_record_info.file_name = file_name;
+            g_record_info.type = RECORD_TEXT_FILE;
+            user_defined_output = true;
         }
 
         if (!export_type.empty()) {
@@ -495,22 +497,17 @@ XRAPI_ATTR XrResult XRAPI_CALL CoreValidationXrCreateApiLayerInstance(const XrIn
             std::transform(export_type.begin(), export_type.end(), export_type_lower.begin(),
                            [](unsigned char c) { return std::tolower(c); });
 
-            std::cerr << "Core Validation output type: " << export_type_lower
-                      << ", first time = " << (first_time ? "true" : "false") << std::endl;
-            if (export_type_lower == "text") {
-                if (!g_record_info.file_name.empty()) {
-                    g_record_info.type = RECORD_TEXT_FILE;
-                } else {
-                    g_record_info.type = RECORD_TEXT_COUT;
-                }
-                user_defined_output = true;
-            } else if (export_type_lower == "html" && first_time) {
+            if (export_type_lower == "html" && first_time) {
                 g_record_info.type = RECORD_HTML_FILE;
                 if (!CoreValidationWriteHtmlHeader()) {
                     return XR_ERROR_INITIALIZATION_FAILED;
                 }
+            } else if (export_type_lower == "none") {
+                g_record_info.type = RECORD_NONE;
             }
         }
+        std::cerr << "Core Validation output type: " << (export_type.empty() ? "text" : export_type)
+                  << ", first time = " << (first_time ? "true" : "false") << std::endl;
 
         // Call the generated pre valid usage check.
         validation_result = GenValidUsageInputsXrCreateInstance(info, instance);
