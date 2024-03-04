e83f35e12cfdc628c0a9e04a0a757e8d3ce4f78a
dengkail <dengkail@qti.qualcomm.com>
2024-03-04 17:31:41 +0800

    OpenXR loader: add helpers for API layer broker discovery

--- src/loader/android_utilities.cpp
+++ src/loader/android_utilities.cpp
@@ -36,8 +36,12 @@ constexpr auto SYSTEM_AUTHORITY = "org.khronos.openxr.system_runtime_broker";
 constexpr auto BASE_PATH = "openxr";
 constexpr auto ABI_PATH = "abi";
 constexpr auto RUNTIMES_PATH = "runtimes";
+constexpr auto API_LAYERS_PATH = "api_layers";
+constexpr auto IMP_LAYER = "implicit";
+constexpr auto EXP_LAYER = "explicit";
 
 constexpr const char *getBrokerAuthority(bool systemBroker) { return systemBroker ? SYSTEM_AUTHORITY : AUTHORITY; }
+constexpr const char *getLayerTypePath(bool implicitLayer) { return implicitLayer ? IMP_LAYER : EXP_LAYER; }
 
 struct BaseColumns {
     /**
@@ -148,6 +152,33 @@ static Uri makeRuntimeContentUri(bool systemBroker, int majorVersion, std::strin
     return builder.build();
 }
 
+/**
+ * Create a content URI for querying all rows of the API Layer function remapping data for a given
+ * runtime package, major version of OpenXR, and layer name,
+ *
+ * @param systemBroker If the system runtime broker (instead of the installable one) should be queried.
+ * @param majorVer    The major version of OpenXR.
+ * @param packageName The package name of the runtime.
+ * @param abi The Android ABI name in use.
+ * @param layerName The name of the API layer.
+ * @return A content URI for the entire table: the function remapping for that runtime and layer.
+ */
+static Uri makeLayerContentUri(bool systemBroker, int majorVersion, std::string const &packageName, const char *abi,
+                               std::string const &layerName) {
+    auto builder = Uri_Builder::construct();
+    builder.scheme("content")
+        .authority(getBrokerAuthority(systemBroker))
+        .appendPath(BASE_PATH)
+        .appendPath(std::to_string(majorVersion))
+        .appendPath(ABI_PATH)
+        .appendPath(abi)
+        .appendPath(API_LAYERS_PATH)
+        .appendPath(packageName)
+        .appendPath(layerName)
+        .appendPath(TABLE_PATH);
+    return builder.build();
+}
+
 struct Columns : BaseColumns {
     /**
      * Constant for the FUNCTION_NAME column name
@@ -160,6 +191,94 @@ struct Columns : BaseColumns {
     static constexpr auto SYMBOL_NAME = "symbol_name";
 };
 }  // namespace functions
+
+namespace instance_extensions {
+/**
+ * Final path component to this URI.
+ */
+static constexpr auto TABLE_PATH = "instance_extensions";
+
+/**
+ * Create a content URI for querying all rows of the instance extensions supported by a given
+ * API layer.
+ *
+ * @param systemBroker If the system runtime broker (instead of the installable one) should be queried.
+ * @param majorVer    The major version of OpenXR.
+ * @param packageName The package name of the runtime.
+ * @param abi The Android ABI name in use.
+ * @param layerName The API layer name.
+ * @return A content URI for the entire table: the function remapping for that runtime.
+ */
+static Uri makeContentUri(bool systemBroker, int majorVersion, std::string const &packageName, const char *abi,
+                          std::string const &layerName) {
+    auto builder = Uri_Builder::construct();
+    builder.scheme("content")
+        .authority(getBrokerAuthority(systemBroker))
+        .appendPath(BASE_PATH)
+        .appendPath(std::to_string(majorVersion))
+        .appendPath(ABI_PATH)
+        .appendPath(abi)
+        .appendPath(API_LAYERS_PATH)
+        .appendPath(packageName)
+        .appendPath(layerName)
+        .appendPath(TABLE_PATH);
+    return builder.build();
+}
+struct Columns : BaseColumns {
+    /**
+     * Constant for the INSTANCE_EXTENSION_NAMES column name
+     */
+    static constexpr auto INSTANCE_EXTENSION_NAMES = "instance_extension_names";
+
+    /**
+     * Constant for the INSTANCE_EXTENSION_VERSIONS column name
+     */
+    static constexpr auto INSTANCE_EXTENSION_VERSIONS = "instance_extension_versions";
+};
+}  // namespace instance_extensions
+
+namespace api_layer {
+
+/**
+ * Create a content URI for querying all rows of the implicit/explicit API layer data for a given
+ * runtime package and major version of OpenXR.
+ *
+ * @param systemBroker If the system runtime broker (instead of the installable one) should be queried.
+ * @param majorVer    The major version of OpenXR.
+ * @param layerType The layer type of the API layer.
+ * @param abi The Android ABI name in use.
+ * @return A content URI for the entire table: the API layers for that runtime.
+ */
+static Uri makeContentUri(bool systemBroker, int majorVersion, std::string const &layerType, const char *abi) {
+    auto builder = Uri_Builder::construct();
+    builder.scheme("content")
+        .authority(getBrokerAuthority(systemBroker))
+        .appendPath(BASE_PATH)
+        .appendPath(std::to_string(majorVersion))
+        .appendPath(ABI_PATH)
+        .appendPath(abi)
+        .appendPath(API_LAYERS_PATH)
+        .appendPath(getLayerTypePath(layerType == IMP_LAYER));
+    return builder.build();
+}
+struct Columns : BaseColumns {
+    // implicit or explicit
+    static constexpr auto PACKAGE_NAME = "package_name";
+    static constexpr auto FILE_FORMAT_VERSION = "file_format_version";
+    static constexpr auto NAME = "name";
+    static constexpr auto NATIVE_LIB_DIR = "native_lib_dir";
+    static constexpr auto SO_FILENAME = "so_filename";
+    static constexpr auto API_VERSION = "api_version";
+    static constexpr auto IMPLEMENTATION_VERSION = "implementation_version";
+    static constexpr auto DESCRIPTION = "description";
+    static constexpr auto DISABLE_ENVIRONMENT = "disable_environment";
+    static constexpr auto ENABLE_ENVIRONMENT = "enable_environment";
+    static constexpr auto DISABLE_SYS_PROP = "disable_sys_prop";
+    static constexpr auto ENABLE_SYS_PROP = "enable_sys_prop";
+    static constexpr auto HAS_FUNCTIONS = "has_functions";
+    static constexpr auto HAS_INSTANCE_EXTENSIONS = "has_instance_extensions";
+};
+}  // namespace api_layer
 
 }  // namespace
 
@@ -332,6 +451,157 @@ int getActiveRuntimeVirtualManifest(wrap::android::content::Context const &conte
     cursor.close();
     return -1;
 }
+
+static int populateApiLayerFunctions(wrap::android::content::Context const &context, bool systemBroker,
+                                     const std::string &packageName, std::string const &layerName, Json::Value &rootNode) {
+    const jni::Array<std::string> projection = makeArray({functions::Columns::FUNCTION_NAME, functions::Columns::SYMBOL_NAME});
+
+    auto uri = functions::makeLayerContentUri(systemBroker, XR_VERSION_MAJOR(XR_CURRENT_API_VERSION), packageName, ABI, layerName);
+    ALOGI("populateApiLayerFunctions: Querying URI: %s", uri.toString().c_str());
+
+    Cursor cursor;
+    if (!getCursor(context, projection, uri, systemBroker, "API layer functions", cursor)) {
+        return -1;
+    }
+
+    auto functionIndex = cursor.getColumnIndex(functions::Columns::FUNCTION_NAME);
+    auto symbolIndex = cursor.getColumnIndex(functions::Columns::SYMBOL_NAME);
+    while (cursor.moveToNext()) {
+        rootNode["api_layer"]["functions"][cursor.getString(functionIndex)] = cursor.getString(symbolIndex);
+    }
+
+    cursor.close();
+    return 0;
+}
+
+static int populateApiLayerInstanceExtensions(wrap::android::content::Context const &context, bool systemBroker,
+                                              const std::string &packageName, std::string const &layerName, Json::Value &rootNode) {
+    const jni::Array<std::string> projection = makeArray(
+        {instance_extensions::Columns::INSTANCE_EXTENSION_NAMES, instance_extensions::Columns::INSTANCE_EXTENSION_VERSIONS});
+
+    auto uri =
+        instance_extensions::makeContentUri(systemBroker, XR_VERSION_MAJOR(XR_CURRENT_API_VERSION), packageName, ABI, layerName);
+    ALOGI("populateApiLayerInstanceExtensions: Querying URI: %s", uri.toString().c_str());
+
+    Cursor cursor;
+    if (!getCursor(context, projection, uri, systemBroker, "API layer instance extensions", cursor)) {
+        return -1;
+    }
+
+    auto nameIndex = cursor.getColumnIndex(instance_extensions::Columns::INSTANCE_EXTENSION_NAMES);
+    auto versionIndex = cursor.getColumnIndex(instance_extensions::Columns::INSTANCE_EXTENSION_VERSIONS);
+    Json::Value extension(Json::objectValue);
+    while (cursor.moveToNext()) {
+        extension["name"] = cursor.getString(nameIndex);
+        extension["extension_version"] = cursor.getString(versionIndex);
+        rootNode["api_layer"]["instance_extensions"].append(extension);
+    }
+
+    cursor.close();
+    return 0;
+}
+
+/// Get cursor for API layers, parameterized by whether or not we use the system broker
+static bool getApiLayerCursor(wrap::android::content::Context const &context, jni::Array<std::string> const &projection,
+                              std::string const &targetType, bool systemBroker, Cursor &cursor) {
+    auto uri = api_layer::makeContentUri(systemBroker, XR_VERSION_MAJOR(XR_CURRENT_API_VERSION), targetType, ABI);
+    ALOGI("getApiLayerCursor: Querying URI: %s", uri.toString().c_str());
+    return getCursor(context, projection, uri, systemBroker, "API Layer", cursor);
+}
+
+static Json::Value makeApiLayerManifest(bool systemBroker, std::string layerType, wrap::android::content::Context const &context,
+                                        Cursor &cursor) {
+    auto packageName = cursor.getString(cursor.getColumnIndex(api_layer::Columns::PACKAGE_NAME));
+    auto fileFormatVersion = cursor.getString(cursor.getColumnIndex(api_layer::Columns::FILE_FORMAT_VERSION));
+    auto name = cursor.getString(cursor.getColumnIndex(api_layer::Columns::NAME));
+    auto libDir = cursor.getString(cursor.getColumnIndex(active_runtime::Columns::NATIVE_LIB_DIR));
+    auto fileName = cursor.getString(cursor.getColumnIndex(api_layer::Columns::SO_FILENAME));
+    auto apiVersion = cursor.getString(cursor.getColumnIndex(api_layer::Columns::API_VERSION));
+    auto implementationVersion = cursor.getString(cursor.getColumnIndex(api_layer::Columns::IMPLEMENTATION_VERSION));
+    auto description = cursor.getString(cursor.getColumnIndex(api_layer::Columns::DESCRIPTION));
+    auto disable_environment = cursor.getString(cursor.getColumnIndex(api_layer::Columns::DISABLE_ENVIRONMENT));
+    auto enable_environment = cursor.getString(cursor.getColumnIndex(api_layer::Columns::ENABLE_ENVIRONMENT));
+    auto disable_sys_prop = cursor.getString(cursor.getColumnIndex(api_layer::Columns::DISABLE_SYS_PROP));
+    auto enable_sys_prop = cursor.getString(cursor.getColumnIndex(api_layer::Columns::ENABLE_SYS_PROP));
+    auto has_instance_extensions = cursor.getString(cursor.getColumnIndex(api_layer::Columns::HAS_INSTANCE_EXTENSIONS));
+    auto has_functions = cursor.getString(cursor.getColumnIndex(api_layer::Columns::HAS_FUNCTIONS));
+
+    ALOGI("Got api layer: type: %s, name: %s, native lib dir: %s, fileName: %s", layerType.c_str(), name.c_str(), libDir.c_str(),
+          fileName.c_str());
+
+    Json::Value rootNode(Json::objectValue);
+    rootNode["file_format_version"] = fileFormatVersion;
+    rootNode["api_layer"] = Json::objectValue;
+    rootNode["api_layer"]["name"] = name;
+    rootNode["api_layer"]["library_path"] = libDir + "/" + fileName;
+    rootNode["api_layer"]["api_version"] = apiVersion;
+    rootNode["api_layer"]["implementation_version"] = implementationVersion;
+    rootNode["api_layer"]["description"] = description;
+    rootNode["api_layer"]["disable_environment"] = disable_environment;
+    rootNode["api_layer"]["enable_environment"] = enable_environment;
+    rootNode["api_layer"]["disable_sys_prop"] = disable_sys_prop;
+    rootNode["api_layer"]["enable_sys_prop"] = enable_sys_prop;
+    if (has_functions == "true") {
+        rootNode["api_layer"]["functions"] = Json::Value(Json::objectValue);
+        populateApiLayerFunctions(context, systemBroker, packageName, name, rootNode);
+    }
+    if (has_instance_extensions == "true") {
+        rootNode["api_layer"]["instance_extensions"] = Json::Value(Json::arrayValue);
+        populateApiLayerInstanceExtensions(context, systemBroker, packageName, name, rootNode);
+    }
+
+    return rootNode;
+}
+
+static int enumerateApiLayerManifests(std::string layerType, wrap::android::content::Context const &context,
+                                      const jni::Array<std::string> &projection, std::vector<Json::Value> &virtualManifests,
+                                      bool systemBroker) {
+    Cursor cursor;
+
+    if (!getApiLayerCursor(context, projection, layerType, systemBroker, cursor)) {
+        return -1;
+    }
+
+    cursor.moveToFirst();
+    const int32_t n = cursor.getCount();
+    virtualManifests.reserve(virtualManifests.size() + n);
+
+    for (int32_t i = 0; i < n; ++i) {
+        virtualManifests.emplace_back(makeApiLayerManifest(systemBroker, layerType, context, cursor));
+        cursor.moveToNext();
+    }
+
+    cursor.close();
+    return 0;
+}
+
+int getApiLayerVirtualManifests(std::string layerType, wrap::android::content::Context const &context,
+                                std::vector<Json::Value> &virtualManifests, bool systemBroker) {
+    ALOGI("Try to get %s API layer from broker", layerType.c_str());
+    const jni::Array<std::string> projection = makeArray(
+        {api_layer::Columns::PACKAGE_NAME, api_layer::Columns::FILE_FORMAT_VERSION, api_layer::Columns::NAME,
+         api_layer::Columns::NATIVE_LIB_DIR, api_layer::Columns::SO_FILENAME, api_layer::Columns::API_VERSION,
+         api_layer::Columns::IMPLEMENTATION_VERSION, api_layer::Columns::DESCRIPTION, api_layer::Columns::DISABLE_ENVIRONMENT,
+         api_layer::Columns::ENABLE_ENVIRONMENT, api_layer::Columns::DISABLE_SYS_PROP, api_layer::Columns::ENABLE_SYS_PROP,
+         api_layer::Columns::HAS_FUNCTIONS, api_layer::Columns::HAS_INSTANCE_EXTENSIONS});
+
+    if (layerType == IMP_LAYER) {
+        std::vector<Json::Value> implicitLayerManifest;
+        if (0 != enumerateApiLayerManifests(IMP_LAYER, context, projection, implicitLayerManifest, systemBroker)) {
+            return -1;
+        }
+        virtualManifests = std::move(implicitLayerManifest);
+    } else {
+        std::vector<Json::Value> explicitLayerManifest;
+        if (0 != enumerateApiLayerManifests(EXP_LAYER, context, projection, explicitLayerManifest, systemBroker)) {
+            return -1;
+        }
+        virtualManifests = std::move(explicitLayerManifest);
+    }
+
+    return 0;
+}
+
 }  // namespace openxr_android
 
 #endif  // __ANDROID__
